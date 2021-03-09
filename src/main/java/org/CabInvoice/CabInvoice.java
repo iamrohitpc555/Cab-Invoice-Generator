fb9f74ac3771eced43456081af245c7f2d9ab928
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-03-09 22:12:15 +0530

    Calculate Toatal Fare

--- src/main/java/org/CabInvoice/CabInvoice.java
+++ src/main/java/org/CabInvoice/CabInvoice.java
@@ -28,6 +28,8 @@ public class CabInvoice
         return TotalFare;
 
     }
+
+
 }
 
 
--- src/test/java/org/CabInvoice/CabInvoiceTest.java
+++ src/test/java/org/CabInvoice/CabInvoiceTest.java
@@ -27,5 +27,14 @@ public class CabInvoiceTest
         Assert.assertEquals(8,fare,0.0);
     }
 
+    @Test
+    public void givenDistanceAndTimeForMultipleRides_ShouldReturnCorrectTotalFare() {
+        CabInvoice invoiceService = new CabInvoice();
+        Ride[] ride = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+        double fare = invoiceService.CalculateFare(ride);
+        Assert.assertEquals(30, fare, 0.0);
+    }
+
 }
 
